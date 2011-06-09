6d06f1c9acf4f514463c56d750f0553173788da5
Michael Strüder <mikezter@gmail.com>
2011-06-09 15:13:01 +0200

    valid multibyte excaping

--- lib/vpim/vcard.rb
+++ lib/vpim/vcard.rb
@@ -26,7 +26,7 @@ module Vpim
   # All line values can be accessed with Vcard#value, Vcard#values, or even by
   # iterating through Vcard#lines. Line types that don't have specific support
   # and non-standard line types ("X-MY-SPECIAL", for example) will be returned
-  # as a String, with any base64 or quoted-printable encoding removed. 
+  # as a String, with any base64 or quoted-printable encoding removed.
   #
   # Specific support exists to return more useful values for the standard vCard
   # types, where appropriate.
@@ -272,7 +272,7 @@ module Vpim
     end
 
     # Represents the value of a TEL field.
-    # 
+    #
     # The value is supposed to be a "X.500 Telephone Number" according to RFC
     # 2426, but that standard is not freely available. Otherwise, anything that
     # looks like a phone number should be OK.
@@ -670,19 +670,19 @@ module Vpim
       end
 
       case string
-        when /^\xEF\xBB\xBF/
+        when /^\\xEF\\xBB\\xBF/
           string = string.sub("\xEF\xBB\xBF", '')
-        when /^\xFE\xFF/
+        when /^\\xFE\\xFF/
           arr = string.unpack('n*')
           arr.shift
           string = arr.pack('U*')
-        when /^\xFF\xFE/
+        when /^\\xFF\\xFE/
           arr = string.unpack('v*')
           arr.shift
           string = arr.pack('U*')
-        when /^\x00B/i
+        when /^\\x00B/i
           string = string.unpack('n*').pack('U*')
-        when /^B\x00/i
+        when /^B\\x00/i
           string = string.unpack('v*').pack('U*')
       end
 
@@ -1381,7 +1381,7 @@ module Vpim
       # Copy the fields from +card+ into self using #add_field. If a block is
       # provided, each Field from +card+ is yielded. The block should return a
       # Field to add, or nil.  The Field doesn't have to be the one yielded,
-      # allowing the field to be copied and modified (see Field#copy) before adding, or 
+      # allowing the field to be copied and modified (see Field#copy) before adding, or
       # not added at all if the block yields nil.
       #
       # The vCard fields BEGIN and END aren't copied, and VERSION, N, and FN are copied
